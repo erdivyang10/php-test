81c9795a5abc653ded279a7252209e3a32f1fc22
Divyang Goswami <erdivyang10@gmail.com>
2019-08-30 13:20:34 +0530

    add a new task for machine-test

---
+++ task1.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(0);
+
+Class Task1
+{
+function sum($argv){
+		if(count(explode(',',$argv))>2){
+				echo 'More than 2 value is not allowed';
+		} else {
+		echo 'Sum = ' . $a = array_sum(explode(',',$argv));
+		
+		}
+	}
+	
+}
+
+$value = getopt(['n']);
+Task1:: sum(5)
+
+?>
